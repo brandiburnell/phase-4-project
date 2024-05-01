182db9538fe4ecbb20c902199b8f1c96a5488762
Brandi Burnell <brandiburnell6@gmail.com>
2024-04-30 20:18:11 -0600

    complete seed file

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,80 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Review, Book
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+
+        print("Deleting all records...")
+        User.query.delete()
+        Book.query.delete()
+        Review.query.delete()
+
         print("Starting seed...")
         # Seed code goes here!
+
+        print('Adding users...')
+        # add User seed data
+        users = []
+        usernames = []
+
+        for i in range(10):
+            username = fake.first_name()
+            while username in usernames:
+                username = fake.first_name()
+            usernames.append(username)
+
+            user = User(
+                username=username,
+                first_name=username,
+                last_name=fake.last_name()
+            )
+
+            users.append(user)
+
+        db.session.add_all(users)
+
+        print('Users added!')
+
+        print('Adding books...')
+        # add Book seed data
+        books = []
+        for i in range (20):
+            book = Book(
+                title=fake.text(max_nb_chars=20),
+                author=fake.name(),
+                description=fake.paragraph(nb_sentences=3),
+                year_published=fake.year()
+            )
+
+            books.append(book)
+
+        db.session.add_all(books)
+
+        print('Books added!')
+
+        # add Review seed data
+        print('Adding books...')
+        reviews = []
+        for i in range (20):
+            review = Review(
+                subject=fake.text(max_nb_chars=20),
+                text=fake.paragraph(nb_sentences=3),
+                rating=fake.random_digit()
+            )
+
+            review.user = rc(users)
+            review.book = rc(books)
+
+            reviews.append(review)
+
+        db.session.add_all(reviews)
+
+        print('Reviews added!')
+
+        print('Committing all data...')
+        db.session.commit()
+
+            
